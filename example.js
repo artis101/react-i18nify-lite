c2860e9e9c4d98f56ff75daac25ef0e9064dac71
Gerben Meyer <g.meyer@zoover.nl>
2016-09-22 10:56:42 +0200

    example fix

--- example.js
+++ example.js
@@ -38,7 +38,7 @@ console.log(I18n.t('application.title'));
 console.log(I18n.t('application.hello', {name: 'Aad'}));
 console.log(I18n.t('export', {count: 0}));
 console.log(I18n.t('application.weird_key'));
-console.log(I18n.t(I18n.t('application', {name: 'Aad'})));
+console.log(I18n.t('application', {name: 'Aad'}));
 
 console.log(I18n.l(1385856000000, { dateFormat: 'date.long' }));
 console.log(I18n.l(Math.PI, { maximumFractionDigits: 2 }));
